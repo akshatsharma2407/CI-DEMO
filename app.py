833c3c71c5a415a4c20eb7680b2f17efcd4c7046
Akshat Sharma <akshatsharma5877@gmail.com>
2025-02-26 18:51:36 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+def subs(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+if __name__ == '__main__':
+    print(add(3, 5), subs(3, 5), multi(3, 5))
